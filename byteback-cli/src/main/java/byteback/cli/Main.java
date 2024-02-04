5d93f3a285051893be92b7ae274a17f4943f7404
Marco Paganoni <paganma@usi.c>
2024-02-04 13:53:44 +0100

    Initialize Project and View

--- byteback-cli/src/main/java/byteback/cli/Main.java
+++ byteback-cli/src/main/java/byteback/cli/Main.java
@@ -1,43 +1,33 @@
 package byteback.cli;
 
-import byteback.analysis.ClassInjector;
-import byteback.analysis.RootResolver;
-import byteback.converter.soottoboogie.Prelude;
 import com.beust.jcommander.ParameterException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import soot.Scene;
-import soot.SootClass;
-import soot.options.Options;
-import soot.util.Chain;
-import soot.util.HashChain;
+import sootup.core.cache.provider.LRUCacheProvider;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaProject;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.language.JavaLanguage;
+import sootup.java.core.views.JavaView;
 
 public class Main {
 
 	public static final Logger log = LoggerFactory.getLogger(Main.class);
-
-	public static final Scene scene = Scene.v();
-
-	public static final Options options = Options.v();
-
-	public static final Prelude prelude = Prelude.v();
-
-	public static final RootResolver resolver = RootResolver.v();
-
-	public static final ClassInjector injector = ClassInjector.v();
 
 	public static void convert(final byteback.cli.Configuration configuration) {
 		final PrintStream output;
 
 		if (configuration.getOutputPath() != null) {
 			final File file = configuration.getOutputPath().toFile();
-
 			try {
 				file.createNewFile();
 				output = new PrintStream(new FileOutputStream(file));
@@ -48,63 +38,30 @@ public class Main {
 		} else {
 			output = System.out;
 		}
-
-		final var task = new ConversionTask(resolver, prelude);
-		output.print(task.run().print());
-		output.close();
 	}
 
-	public static void initialize(final byteback.cli.Configuration configuration) {
-		final List<Path> classPaths = configuration.getClassPaths();
+	public static String concatenateClassPaths(final List<Path> classPaths) {
+		String finalClassPath = "";
+		final Iterator<Path> classPathIterator = classPaths.iterator();
+		while (classPathIterator.hasNext()) {
+			final Path classPath = classPathIterator.next();
+			if (!classPathIterator.hasNext()) {
+				finalClassPath += File.pathSeparator;
+			}
+			finalClassPath += classPath.toString();
+		}
+		return finalClassPath;
+	}
+
+	public static JavaProject initializeProject(final byteback.cli.Configuration configuration) {
 		final List<String> startingClassNames = configuration.getStartingClasses();
+		final List<Path> classPaths = configuration.getClassPaths();
+		final String classPath = concatenateClassPaths(classPaths);
 		final Path preludePath = configuration.getPreludePath();
-
-		options.set_allow_phantom_refs(true);
-		options.set_keep_line_number(true);
-		options.set_keep_offset(true);
-		scene.allowsPhantomRefs();
-
-		options.allow_phantom_refs();
-		options.setPhaseOption("jb", "use-original-names:true");
-		options.setPhaseOption("gb.a1", "enabled:false");
-		options.setPhaseOption("gb.cf", "enabled:false");
-		options.setPhaseOption("gb.a2", "enabled:false");
-		options.setPhaseOption("gb.ule", "enabled:false");
-
-		for (final Path classPath : classPaths) {
-			scene.setSootClassPath(scene.getSootClassPath() + File.pathSeparator + classPath);
-		}
-
-		scene.loadBasicClasses();
-
-		final Chain<SootClass> startingClasses = new HashChain<>();
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.KotlinIntrinsicsSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.ObjectSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.ExceptionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.CollectionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.InvokeDynamicSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.IndexOutOfBoundsExceptionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.ConcurrentModificationExceptionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.NoSuchElementExceptionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.IllegalStateExceptionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.IllegalArgumentExceptionSpec"));
-		startingClasses.add(scene.loadClassAndSupport("byteback.annotations.NullPointerExceptionSpec"));
-
-		for (final String startingClassName : startingClassNames) {
-			final SootClass startingClass = scene.loadClassAndSupport(startingClassName);
-			startingClasses.add(startingClass);
-		}
-
-		injector.inject(scene.getClasses());
-		resolver.setCheckNullDereference(Configuration.v().getTransformNullCheck());
-		resolver.setCheckArrayDereference(Configuration.v().getTransformArrayCheck());
-		resolver.resolve(startingClasses);
-
-		if (preludePath != null) {
-			prelude.loadFile(preludePath);
-		} else {
-			prelude.loadDefault();
-		}
+		final JavaLanguage javaLanguage = new JavaLanguage(22);
+		final AnalysisInputLocation<JavaSootClass> inputLocation = 
+			new JavaClassPathAnalysisInputLocation(classPath);
+		return JavaProject.builder(javaLanguage).addInputLocation(inputLocation).build();
 	}
 
 	public static void main(final String[] args) {
@@ -117,7 +74,8 @@ public class Main {
 			if (config.getHelp()) {
 				config.getJCommander().usage();
 			} else {
-				initialize(config);
+				final JavaProject project = initializeProject(config);
+				final JavaView view = project.createView(new LRUCacheProvider<>(50));
 				final long conversionStart = System.currentTimeMillis();
 				log.info("Converting classes");
 				convert(config);
