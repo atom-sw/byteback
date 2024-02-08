1a5858fd5f5f5c13f2d7bcc00010cce6593ed80c
Marco Paganoi <paganma@usi.ch>
2024-02-08 12:12:37 +0100

    Implement comparator cases

--- byteback-sootup/src/main/java/sootup/core/jimple/basic/SpecComparator.java
+++ byteback-sootup/src/main/java/sootup/core/jimple/basic/SpecComparator.java
@@ -1,23 +1,18 @@
 package sootup.core.jimple.basic;
 
-import sootup.core.jimple.basic.JimpleComparator;
 import sootup.core.jimple.stmt.JAssertStmt;
 import sootup.core.jimple.stmt.JInvariantStmt;
 
 public class SpecComparator extends JimpleComparator {
 
 	public boolean caseAssertStmt(final Object o, final JAssertStmt assertStmt) {
-		if (o instanceof JAssertStmt otherAssertStmt) {
-		}
-
-		return false;
+		return (o instanceof JAssertStmt otherAssertStmt)
+				&& assertStmt.getSpecification().equivTo(assertStmt.getSpecification());
 	}
 
 	public boolean caseInvariantStmt(final Object o, final JInvariantStmt invariantStmt) {
-		if (o instanceof JAssertStmt otherAssertStmt) {
-		}
-
-		return false;
+		return (o instanceof JInvariantStmt otherInvariantSmt)
+				&& invariantStmt.getSpecification().equivTo(otherInvariantSmt.getSpecification());
 	}
 
 }
