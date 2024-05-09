8ad811edf9e35a61f413e4ad65b1f882a03888bc
Marco Paganoi <paganma@usi.ch>
2024-05-09 16:43:21 +0200

    Implement HeapType

---
+++ byteback-syntax/src/main/java/byteback/syntax/scene/type/HeapType.java
@@ -0,0 +1,28 @@
+package byteback.syntax.scene.type;
+
+import byteback.common.function.Lazy;
+import soot.Type;
+
+
+/**
+ * A type for a heap reference.
+ *
+ * @author paganma
+ */
+public class HeapType extends Type implements DefaultCaseType {
+
+    private static final Lazy<HeapType> INSTANCE = Lazy.from(HeapType::new);
+
+    public static HeapType v() {
+        return INSTANCE.get();
+    }
+
+    private HeapType() {
+    }
+
+    @Override
+    public String toString() {
+        return "heap";
+    }
+
+}
