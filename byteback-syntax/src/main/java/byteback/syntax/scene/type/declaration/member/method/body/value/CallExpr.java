ff1fbe3f9a515f4d4e2f7d6d2373b3bcd22f872e
Marco Paganoi <paganma@usi.ch>
2024-05-09 16:42:11 +0200

    Fix indentation

--- byteback-syntax/src/main/java/byteback/syntax/scene/type/declaration/member/method/body/value/CallExpr.java
+++ byteback-syntax/src/main/java/byteback/syntax/scene/type/declaration/member/method/body/value/CallExpr.java
@@ -18,83 +18,83 @@ import java.util.ListIterator;
  */
 public class CallExpr extends AbstractInvokeExpr implements DefaultCaseValue {
 
-    public CallExpr(final SootMethodRef methodRef, final List<Value> args) {
-        super(methodRef, new ValueBox[args.size()]);
+	public CallExpr(final SootMethodRef methodRef, final List<Value> args) {
+		super(methodRef, new ValueBox[args.size()]);
 
-        for (final ListIterator<? extends Value> valueIterator = args.listIterator(); valueIterator.hasNext();) {
-            final Value value = valueIterator.next();
-            this.argBoxes[valueIterator.previousIndex()] = Vimp.v().newImmediateBox(value);
-        }
-    }
+		for (final ListIterator<? extends Value> valueIterator = args.listIterator(); valueIterator.hasNext();) {
+			final Value value = valueIterator.next();
+			this.argBoxes[valueIterator.previousIndex()] = Vimp.v().newImmediateBox(value);
+		}
+	}
 
-    @Override
-    public Object clone() {
-        final int count = getArgCount();
-        final List<Value> clonedArgs = new ArrayList<>(count);
+	@Override
+	public Object clone() {
+		final int count = getArgCount();
+		final List<Value> clonedArgs = new ArrayList<>(count);
 
-        for (int i = 0; i < count; i++) {
-            clonedArgs.add(Jimple.cloneIfNecessary(getArg(i)));
-        }
+		for (int i = 0; i < count; i++) {
+			clonedArgs.add(Jimple.cloneIfNecessary(getArg(i)));
+		}
 
-        return new CallExpr(methodRef, clonedArgs);
-    }
+		return new CallExpr(methodRef, clonedArgs);
+	}
 
-    @Override
-    public boolean equivTo(final Object object) {
-        if (object instanceof CallExpr callExpr) {
-            if ((this.argBoxes == null ? 0 : this.argBoxes.length)
-                    != (callExpr.argBoxes == null ? 0 : callExpr.argBoxes.length)
-                    || !this.getMethod().equals(callExpr.getMethod())) {
-                return false;
-            }
+	@Override
+	public boolean equivTo(final Object object) {
+		if (object instanceof CallExpr callExpr) {
+			if ((this.argBoxes == null ? 0 : this.argBoxes.length) != (callExpr.argBoxes == null ? 0
+					: callExpr.argBoxes.length)
+					|| !this.getMethod().equals(callExpr.getMethod())) {
+				return false;
+			}
 
-            if (this.argBoxes != null) {
-                for (int i = 0, e = this.argBoxes.length; i < e; i++) {
-                    if (!this.argBoxes[i].getValue().equivTo(callExpr.argBoxes[i].getValue())) {
-                        return false;
-                    }
-                }
-            }
+			if (this.argBoxes != null) {
+				for (int i = 0, e = this.argBoxes.length; i < e; i++) {
+					if (!this.argBoxes[i].getValue().equivTo(callExpr.argBoxes[i].getValue())) {
+						return false;
+					}
+				}
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public int equivHashCode() {
-        return getMethod().equivHashCode() * 17;
-    }
+	@Override
+	public int equivHashCode() {
+		return getMethod().equivHashCode() * 17;
+	}
 
-    @Override
-    public void toString(final UnitPrinter printer) {
-        printer.literal("(");
-        printer.literal(methodRef.getName());
+	@Override
+	public void toString(final UnitPrinter printer) {
+		printer.literal("(");
+		printer.literal(methodRef.getName());
 
-        if (getArgCount() > 0) {
-            printer.literal(" ");
-            final Iterator<Value> argIterator = getArgs().iterator();
+		if (getArgCount() > 0) {
+			printer.literal(" ");
+			final Iterator<Value> argIterator = getArgs().iterator();
 
-            while (argIterator.hasNext()) {
-                final Value arg = argIterator.next();
-                arg.toString(printer);
+			while (argIterator.hasNext()) {
+				final Value arg = argIterator.next();
+				arg.toString(printer);
 
-                if (argIterator.hasNext()) {
-                    printer.literal(" ");
-                }
-            }
-        }
+				if (argIterator.hasNext()) {
+					printer.literal(" ");
+				}
+			}
+		}
 
-        printer.literal(")");
-    }
+		printer.literal(")");
+	}
 
-    @Override
-    public String toString() {
-        final var unitPrinter = new InlineUnitPrinter();
-        toString(unitPrinter);
+	@Override
+	public String toString() {
+		final var unitPrinter = new InlineUnitPrinter();
+		toString(unitPrinter);
 
-        return unitPrinter.toString();
-    }
+		return unitPrinter.toString();
+	}
 
 }
