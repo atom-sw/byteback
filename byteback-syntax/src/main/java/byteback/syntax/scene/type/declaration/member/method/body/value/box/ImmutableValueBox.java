c683adba7fa9b37b953f0deb7832eae574e0f2b3
Marco Paganoi <paganma@usi.ch>
2024-05-09 16:42:27 +0200

    Fix indentation

--- byteback-syntax/src/main/java/byteback/syntax/scene/type/declaration/member/method/body/value/box/ImmutableValueBox.java
+++ byteback-syntax/src/main/java/byteback/syntax/scene/type/declaration/member/method/body/value/box/ImmutableValueBox.java
@@ -5,18 +5,18 @@ import soot.Value;
 
 public class ImmutableValueBox extends AbstractValueBox {
 
-    public ImmutableValueBox(final Value value) {
-        this.value = value;
-    }
+	public ImmutableValueBox(final Value value) {
+		this.value = value;
+	}
 
-    @Override
-    public boolean canContainValue(final Value value) {
-        return false;
-    }
+	@Override
+	public boolean canContainValue(final Value value) {
+		return false;
+	}
 
-    @Override
-    public void setValue(final Value value) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public void setValue(final Value value) {
+		throw new UnsupportedOperationException();
+	}
 
 }
