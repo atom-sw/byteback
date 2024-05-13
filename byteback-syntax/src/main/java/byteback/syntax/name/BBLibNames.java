b4e10978ff7d31711f763b84665eeac716ed4198
Marco Paganoni <paganma@usi.ch>
2024-05-13 13:22:59 +0200

    Fix the indentation; re-order static fields

--- byteback-syntax/src/main/java/byteback/syntax/name/BBLibNames.java
+++ byteback-syntax/src/main/java/byteback/syntax/name/BBLibNames.java
@@ -10,85 +10,90 @@ import soot.SootClass;
  */
 public class BBLibNames {
 
-    public static final Lazy<BBLibNames> INSTANCE = Lazy.from(BBLibNames::new);
+	public static final Lazy<BBLibNames> INSTANCE = Lazy.from(BBLibNames::new);
 
-    private BBLibNames() {
-    }
+	private BBLibNames() {
+	}
 
-    public static BBLibNames v() {
-        return INSTANCE.get();
-    }
+	public static BBLibNames v() {
+		return INSTANCE.get();
+	}
 
-    public static final String ATTACH_ANNOTATION = "Lbyteback/specification/ghost/Ghost$Attach;";
+	/** Ghost code annotations */
+	public static final String ATTACH_ANNOTATION = "Lbyteback/specification/ghost/Ghost$Attach;";
 
-    public static final String EXPORT_ANNOTATION = "Lbyteback/specification/ghost/Ghost$Export;";
+	public static final String EXPORT_ANNOTATION = "Lbyteback/specification/ghost/Ghost$Export;";
 
-    public static final String IMPORT_ANNOTATION = "Lbyteback/specification/ghost/Ghost$Import;";
+	public static final String IMPORT_ANNOTATION = "Lbyteback/specification/ghost/Ghost$Import;";
 
-    public static final String BEHAVIOR_ANNOTATION = "Lbyteback/specification/Contract$Behavior;";
+	/** Behavior annotations */
+	public static final String BEHAVIOR_ANNOTATION = "Lbyteback/specification/Contract$Behavior;";
 
-    public static final String TWOSTATE_ANNOTATION = "Lbyteback/specification/Contract$TwoState;";
+	public static final String TWOSTATE_ANNOTATION = "Lbyteback/specification/Contract$TwoState;";
 
-    public static final String EXCEPTIONAL_ANNOTATION = "Lbyteback/specification/Contract$Exceptional;";
+	public static final String EXCEPTIONAL_ANNOTATION = "Lbyteback/specification/Contract$Exceptional;";
 
-    public static final String BINDINGS_CLASS_NAME = "byteback.specification.Bindings";
+	public static final String OPERATOR_ANNOTATION = "Lbyteback/specification/Contract$Operator;";
 
-    public static final String CONTRACT_CLASS_NAME = "byteback.specification.Contract";
+	public static final String PRELUDE_ANNOTATION = "Lbyteback/specification/Contract$Prelude;";
 
-    public static final String ENSURES_ANNOTATION = "Lbyteback/specification/Contract$Ensures;";
+	/** Behavior body constructs */
+	public static final String BINDINGS_CLASS_NAME = "byteback.specification.Bindings";
 
-    public static final String ENSURE_ANNOTATION = "Lbyteback/specification/Contract$Ensure;";
+	public static final String CONTRACT_CLASS_NAME = "byteback.specification.Contract";
 
-    public static final String IGNORE_ANNOTATION = "Lbyteback/specification/Contract$Ignore;";
+	public static final String QUANTIFIERS_CLASS_NAME = "byteback.specification.Quantifiers";
 
-    public static final String INVARIANT_NAME = "invariant";
+	public static final String EXISTENTIAL_QUANTIFIER_NAME = "exists";
 
-    public static final String ASSERTION_NAME = "assertion";
+	public static final String UNIVERSAL_QUANTIFIER_NAME = "forall";
 
-    public static final String ASSUMPTION_NAME = "assumption";
+	public static final String SPECIAL_CLASS_NAME = "byteback.specification.Special";
 
-    public static final String OPERATOR_ANNOTATION = "Lbyteback/specification/Contract$Operator;";
+	public static final String CONDITIONAL_NAME = "conditional";
 
-    public static final String PRELUDE_ANNOTATION = "Lbyteback/specification/Contract$Prelude;";
+	public static final String OLD_NAME = "old";
 
-    public static final String QUANTIFIERS_CLASS_NAME = "byteback.specification.Quantifiers";
+	public static final String THROWN_NAME = "thrown";
 
-    public static final String EXISTENTIAL_QUANTIFIER_NAME = "exists";
+	/** Procedure annotations */
+	public static final String ENSURES_ANNOTATION = "Lbyteback/specification/Contract$Ensures;";
 
-    public static final String UNIVERSAL_QUANTIFIER_NAME = "forall";
+	public static final String ENSURE_ANNOTATION = "Lbyteback/specification/Contract$Ensure;";
 
-    public static final String RAISE_ANNOTATION = "Lbyteback/specification/Contract$Raise;";
+	public static final String IGNORE_ANNOTATION = "Lbyteback/specification/Contract$Ignore;";
 
-    public static final String REQUIRE_ANNOTATION = "Lbyteback/specification/Contract$Require;";
+	public static final String ABSTRACT_ANNOTATION = "Lbyteback/specification/Contract$Abstract;";
 
-    public static final String RETURN_ANNOTATION = "Lbyteback/specification/Contract$Return;";
+	public static final String RAISE_ANNOTATION = "Lbyteback/specification/Contract$Raise;";
 
-    public static final String ABSTRACT_ANNOTATION = "Lbyteback/specification/Contract$Abstract;";
+	public static final String REQUIRE_ANNOTATION = "Lbyteback/specification/Contract$Require;";
 
-    public static final String PURE_ANNOTATION = "Lbyteback/specification/Contract$Pure;";
+	public static final String RETURN_ANNOTATION = "Lbyteback/specification/Contract$Return;";
 
-    public static final String SPECIAL_CLASS_NAME = "byteback.specification.Special";
+	public static final String PURE_ANNOTATION = "Lbyteback/specification/Contract$Pure;";
 
-    public static final String CONDITIONAL_NAME = "conditional";
+	/** Procedural specification constructs */
+	public static final String INVARIANT_NAME = "invariant";
 
-    public static final String OLD_NAME = "old";
+	public static final String ASSERTION_NAME = "assertion";
 
-    public static final String THROWN_NAME = "thrown";
+	public static final String ASSUMPTION_NAME = "assumption";
 
-    public boolean isBindingsClass(final SootClass sootClass) {
-        return sootClass.getName().equals(BINDINGS_CLASS_NAME);
-    }
+	public boolean isBindingsClass(final SootClass sootClass) {
+		return sootClass.getName().equals(BINDINGS_CLASS_NAME);
+	}
 
-    public boolean isContractClass(final SootClass sootClass) {
-        return sootClass.getName().equals(CONTRACT_CLASS_NAME);
-    }
+	public boolean isContractClass(final SootClass sootClass) {
+		return sootClass.getName().equals(CONTRACT_CLASS_NAME);
+	}
 
-    public boolean isQuantifiersClass(final SootClass sootClass) {
-        return sootClass.getName().equals(QUANTIFIERS_CLASS_NAME);
-    }
+	public boolean isQuantifiersClass(final SootClass sootClass) {
+		return sootClass.getName().equals(QUANTIFIERS_CLASS_NAME);
+	}
 
-    public boolean isSpecialClass(final SootClass sootClass) {
-        return sootClass.getName().equals(SPECIAL_CLASS_NAME);
-    }
+	public boolean isSpecialClass(final SootClass sootClass) {
+		return sootClass.getName().equals(SPECIAL_CLASS_NAME);
+	}
 
 }
