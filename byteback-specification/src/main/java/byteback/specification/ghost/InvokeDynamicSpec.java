36ec0dd965e5df320bcccd21c6110ed1b8f22297
Marco Paganoni <paganma@usi.ch>
2024-05-14 11:06:14 +0200

    Export InvokeDynamicSpec methods

--- byteback-specification/src/main/java/byteback/specification/ghost/InvokeDynamicSpec.java
+++ byteback-specification/src/main/java/byteback/specification/ghost/InvokeDynamicSpec.java
@@ -3,6 +3,7 @@ package byteback.specification.ghost;
 import byteback.specification.Contract.Abstract;
 import byteback.specification.Contract.Return;
 import byteback.specification.ghost.Ghost.Attach;
+import byteback.specification.ghost.Ghost.Export;
 
 @Attach("soot.dummy.InvokeDynamic")
 public abstract class InvokeDynamicSpec {
@@ -12,12 +13,14 @@ public abstract class InvokeDynamicSpec {
 	}
 
 	@Return
+	@Export
 	@Abstract
 	public static String makeConcatWithConstants(int fromIndex, int toIndex) {
 		throw new UnsupportedOperationException();
 	}
 
 	@Return
+	@Export
 	@Abstract
 	public static String makeConcatWithConstants(int index) {
 		throw new UnsupportedOperationException();
