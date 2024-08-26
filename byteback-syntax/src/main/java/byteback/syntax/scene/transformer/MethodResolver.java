08d1c462e81ab5f2b14580e84c4965184493160d
Marco Paganoi <paganma@usi.ch>
2024-08-26 15:29:16 +0200

    Add MethodResolver

---
+++ byteback-syntax/src/main/java/byteback/syntax/scene/transformer/MethodResolver.java
@@ -0,0 +1,63 @@
+package byteback.syntax.scene.transformer;
+
+import byteback.common.function.Lazy;
+import soot.Body;
+import soot.Scene;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.SootMethodRef;
+import soot.Value;
+import soot.ValueBox;
+import soot.jimple.InvokeExpr;
+
+public class MethodResolver extends SceneTransformer {
+
+	private static final Lazy<MethodResolver> INSTANCE = Lazy.from(MethodResolver::new);
+
+	public static MethodResolver v() {
+		return INSTANCE.get();
+	}
+
+	private MethodResolver() {
+	}
+
+	public void ensureExists(final Scene scene, final SootMethodRef methodRef) {
+		final SootMethod sootMethod = methodRef.tryResolve();
+
+		if (sootMethod == null) {
+			final SootClass declaringClass = methodRef.getDeclaringClass();
+			final var newSootMethod = new SootMethod(
+					methodRef.getName(),
+					methodRef.getParameterTypes(),
+					methodRef.getReturnType());
+			declaringClass.addMethod(newSootMethod);
+		}
+	}
+
+	@Override
+	public void transformScene(final Scene scene) {
+		for (final SootClass sootClass : scene.getClasses()) {
+			if (sootClass.resolvingLevel() < SootClass.BODIES) {
+				continue;
+			}
+
+			for (final SootMethod sootMethod : sootClass.getMethods()) {
+				if (sootMethod.isAbstract()) {
+					continue;
+				}
+
+				final Body body = sootMethod.retrieveActiveBody();
+
+				for (final ValueBox valueBox : body.getUseBoxes()) {
+					final Value value = valueBox.getValue();
+
+					if (value instanceof final InvokeExpr invokeExpr) {
+						final SootMethodRef methodRef = invokeExpr.getMethodRef();
+						ensureExists(scene, methodRef);
+					}
+				}
+			}
+		}
+	}
+
+}
